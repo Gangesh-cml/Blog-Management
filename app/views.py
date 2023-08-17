4e07cb05378a2d952077ce789ab04923af3ca1a6
Gangesh-cml <gangesh.kumar@crossml.com>
2023-08-17 16:41:08 +0530

    updated views.py

--- app/views.py
+++ app/views.py
@@ -8,36 +8,50 @@ from django.contrib import messages
 from .models import Profile
 from .helper import send_forget_password_mail
 from .models import *
+from django.contrib.auth.models import Group
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def loginpage(request):
     if request.method=='POST':
         username=request.POST['username']
         password=request.POST['password']
-      
-        if not username or not password:
-            messages.success(request,'Both Username and password are required')
-            return redirect('/login/')
-        # user_obj=User.objects.filter(username=username).first()
-        # if user_obj is None:
-        #     messages.success(request,'User not found.')
-        #     return redirect('/login/')
+        
         if (User.objects.filter(username=username).exists()):
+            user_obj=User.objects.filter(username=username).first()
+            profile_obj=Profile.objects.filter(user=user_obj).first()
             user=authenticate(request,username=username, password=password)
-        else:
+        
+        elif(User.objects.filter(email=username).exists()):
             user=User.objects.get(email=username)
-            print(user)
+            profile_obj=Profile.objects.filter(user=user).first()
             user=authenticate(username=user.username,password=password)
-        if user is not None:
+        
+        else:
+            messages.success(request,'User not found.')
+            return redirect('/login/')
+
+
+        if user.is_superuser:
             login(request,user)
-            # messages.success(request,f"You are logged in as {username}")
             return redirect('/dashboard/')
         else:
-            messages.success(request,f"Invaild Credentials")
+            if not profile_obj.is_verified:
+                messages.success(request,'profile is not verified please check your mail.')
+                return redirect('/login')
+            
+        if user is not None:
+            login(request,user)
+            return redirect('/dashboard/')
+        
+        # else:
+        #     messages.success(request,f"Invaild Credentials")
            
 
 
     return render(request,'loginpage.html')
+
 
 # this function is used to logout the user
 def logout_user(request):
@@ -46,6 +60,7 @@ def logout_user(request):
     messages.success(request,("You Were Successfully logout"))
     return redirect('login')
 
+
 # this function is used to register the new user
 def register(request):
     if request.method=='POST':
@@ -53,7 +68,6 @@ def register(request):
         email=request.POST['email']
         password1=request.POST['password1']
         password2=request.POST['password2']
-        print(username)
         try:
             if User.objects.filter(username=username).first():
                 messages.success(request,'Username is taken.')
@@ -68,9 +82,11 @@ def register(request):
             else:
                 data=User.objects.create_user(username=username,email=email,password=password1)
                 data.save()
-                profile_obj=Profile.objects.create(user=data)
+                token=str(uuid.uuid4())
+                profile_obj=Profile.objects.create(user=data , email_token=token)
                 profile_obj.save()
-                return redirect('/')
+                send_mail_after_registeration(email,token)
+                return redirect('/login/')
         except Exception as e:
             print(e)
     
@@ -83,21 +99,15 @@ def forget_password(request):
         if request.method=='POST':
             username=request.POST.get('username')
             email= request.POST.get("email")
-            print(email)
-            # print(username)
             if not User.objects.filter(email=email).first():
                 messages.success(request,'User not found with this username.')
                 return redirect('/forget-password/')
             user_obj=User.objects.get(email=email)
-            print(user_obj)
             token=str(uuid.uuid4())
             profile_obj=Profile.objects.get(user=user_obj)
-            print(profile_obj)
             profile_obj.forget_password_token=token
             profile_obj.save()
-            print(user_obj)
             send_forget_password_mail(email,token)
-            print(token)
             messages.success(request,'An email is sent')
             return redirect('/forget-password/')
 
@@ -106,17 +116,18 @@ def forget_password(request):
 
     return render(request,'forget_password.html')
 
+
+
 def change_password(request,token):
     context={}
     profile_obj=Profile.objects.filter(forget_password_token=token).first()
-    print(profile_obj)
     if request.method=='POST':
         password1=request.POST['password']
         password2=request.POST['confirm_password']
         user_id=request.POST.get('user_id')
         if user_id is None:
             messages.success(request,"No user id found.")
-            return redirect(f'/changep-passeord/{token}')
+            return redirect(f'/change-passeord/{token}')
         if password1 != password2:
             messages.success(request,"both should be equal.")
             return redirect('/')
@@ -130,68 +141,43 @@ def change_password(request,token):
     return render(request,'change-password.html',context)
 
 
-def is_superuser(user):
-    return user.is_superuser
 
+#for mail verification
 
-# class PasswordChangeView(PasswordChangeView)
+def send_mail_after_registeration(email,token):
+    subject='your forget password link'
+    message=f'hi,click on the link to verify your account http://127.0.0.1:8000/verify/{token}'
+    email_from=settings.EMAIL_HOST_USER
+    recipient_list=[email]
+    send_mail(subject,message,email_from,recipient_list)
+    return True
 
-
-
-# from django.contrib.auth.tokens import default_token_generator
-# from django.contrib.auth import get_user_model
-# from django.utils import timezone
-# from django.core.signing import TimestampSigner
-
-# def send_reset_password_email(request):
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         user = get_user_model().objects.get(email=email)
-
-#         if user:
-#             # Generate a timestamped token for the user's email
-#             timestamp = int(timezone.now().timestamp())
-#             signer = TimestampSigner()
-#             token = signer.sign_object({'email': email, 'timestamp': timestamp})
-
-#             # Send the email with the reset link
-#             reset_link = f'http://example.com/reset-password/?token={token}'
-#             # Replace 'example.com' with your actual domain name
-#             # Here, you can use Django's email sending mechanism or any third-party library.
-
-#             return HttpResponse('Reset password link sent.')
-
-
-# # views.py
-# from django.core.signing import BadSignature, SignatureExpired
-
-# def reset_password(request):
-#     token = request.GET.get('token')
-#     try:
-#         signer = TimestampSigner()
-#         data = signer.unsign_object(token, max_age=PASSWORD_RESET_TIMEOUT_DAYS * 24 * 3600)
-#         email = data['email']
-#         timestamp = data['timestamp']
-
-#         # Check if the token is expired
-#         if timezone.now().timestamp() - timestamp > PASSWORD_RESET_TIMEOUT_DAYS * 24 * 3600:
-#             return HttpResponse('Reset password link has expired.')
-
-#         # Process the password reset here
-#         # ...
-
-#         return HttpResponse('Password reset successful.')
-
-#     except (BadSignature, SignatureExpired):
-#         return HttpResponse('Invalid reset password link.')
+def verify(request, email_token):
+    try:
+        Profile_obj=Profile.objects.filter(email_token=email_token).first()
+        if Profile_obj:
+            if Profile_obj.is_verified:
+                messages.success(request,'your acoount is already verified')
+                return redirect('/login/')
+            
+            Profile_obj.is_verified = True
+            Profile_obj.save()
+            messages.success(request,'your acoount has been verified')
+            return redirect('/login/')
+        else:
+            return messages.success(request,'link is invailid')
+    except Exception as e :
+        print(e)
 
 
 
 def is_superuser(user):
     return user.is_superuser
+
 
 from django.shortcuts import render
 from .forms import AssignPermissionForm
+
 
 def assign_permission_view(request):
     if request.method == 'POST':
@@ -199,11 +185,15 @@ def assign_permission_view(request):
         if form.is_valid():
             user = form.cleaned_data['user']
             group = form.cleaned_data['group']
+            print(group)
             permissions = form.cleaned_data['permissions']
 
             if user:
                 user.user_permissions.set(permissions)
-            elif group:
+            if group:
+                
+                group1= Group.objects.get(name=group)
+                user.groups.add(group1)
                 group.permissions.set(permissions)
 
             return render(request, 'success.html')
