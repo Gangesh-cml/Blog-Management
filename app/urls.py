e0cc8306e54842a94a2b98472a5c6f7dc95a6756
Gangesh-cml <gangesh.kumar@crossml.com>
2023-08-17 16:40:45 +0530

    updated urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,11 @@ from app import views
 
 
 urlpatterns = [
-    # path('home/',views.home,name='home'),
     path('login/',views.loginpage,name='login'),
-    # path('add/',views.add_blog,name='blog'),
     path('logout/',views.logout_user,name='logout'),
     path('register/',views.register,name='register'),
     path('forget-password/',views.forget_password,name='forget-password'),
     path('change-password/<token>/',views.change_password,name='change-password'),
     path('assign_permissions/', views.assign_permission_view, name='assign_permissions'),
+    path('verify/<email_token>',views.verify,name='verify')
 ]
